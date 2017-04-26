090e0b3e41e50bcbd691cef639ed3a2a80c81ff8
flex2424 <fakestratocaster@yandex.ru>
2017-04-26 20:40:16 +0300

    combobox certificates

--- CryptoProClient/CryptoProClient/Form1.cs
+++ CryptoProClient/CryptoProClient/Form1.cs
@@ -22,9 +22,8 @@ namespace CryptoProClient
 {
     public partial class Form1 : Form
     {
-        string container_name = "le-4cd9f341-5657-4431-99f7-c7c4f33de108";
-        Gost3410 signature;
-        byte[] command;
+        //string container_name = "le-4cd9f341-5657-4431-99f7-c7c4f33de108";
+        byte[] mode;
 
         public Form1()
         {
@@ -33,7 +32,36 @@ namespace CryptoProClient
 
         private void Form1_Load(object sender, EventArgs e)
         {
-         
+            mode = new byte[2];
+            mode[0] = 0x01;
+            mode[1] = 0x02;
+
+            foreach (StoreLocation storeLocation in (StoreLocation[])
+                Enum.GetValues(typeof(StoreLocation)))
+                if (storeLocation.ToString() == "CurrentUser")
+                    foreach (StoreName storeName in (StoreName[])
+                        Enum.GetValues(typeof(StoreName)))
+                    {
+                        if (storeName.ToString() == "My")
+                        {
+                            // Выводим все сертификаты в /Текущий пользователь/Личное
+                            X509Store store = new X509Store(storeName, storeLocation);
+
+                            try
+                            {
+                                store.Open(OpenFlags.OpenExistingOnly);
+                                foreach (X509Certificate2 cert in store.Certificates)
+                                {
+                                    comboBox1.Items.Add(cert.GetName());
+                                }
+                            }
+                            catch (CryptographicException)
+                            {
+                                MessageBox.Show("Exception in comboBox");
+                            }
+                        }
+                    }
+
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -85,6 +113,8 @@ namespace CryptoProClient
             cs.Close();
             ms.Close();
 
+            send_data("encryption", cipher_text_bytes);
+
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
@@ -101,7 +131,8 @@ namespace CryptoProClient
             Gost3411CryptoServiceProvider hash = new Gost3411CryptoServiceProvider();
 
             byte[] signature = gost.SignData(plain_text_bytes, hash);
-            
+
+            send_data("signature", signature);
 
         }
 
@@ -132,5 +163,46 @@ namespace CryptoProClient
            X509Certificate2 cert = found[0];
            return cert;
         }
+
+        private void send_data(string flag, byte[] data)
+        {
+            try
+            {
+                Int32 port = 9595;
+                TcpClient client = new TcpClient("127.0.0.1", port);
+                NetworkStream stream = client.GetStream();
+                if (flag == "encryption")
+                {
+                    stream.Write(mode, 0, 1);
+                    MessageBox.Show("ecnrypt");
+                }
+                else
+                {
+                    stream.Write(mode, 1, 1);
+                    MessageBox.Show("sign");
+                }
+                stream.Write(data, 0, data.Length);
+
+                data = new Byte[256];
+                String responseData = String.Empty;
+
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                MessageBox.Show("Received: " + responseData);
+
+                stream.Close();
+                client.Close();
+
+            }
+            catch (ArgumentNullException exception)
+            {
+                MessageBox.Show("ArgumentNullException: " + exception);
+            }
+            catch (SocketException exception)
+            {
+                MessageBox.Show("SocketException: " + exception);
+            }
+        }
+        
     }
 }
